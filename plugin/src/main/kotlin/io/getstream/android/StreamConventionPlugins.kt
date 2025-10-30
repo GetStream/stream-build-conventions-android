3f3f3703d2adb19394b312cc8dcb1f3b137cc538
Gian <47775302+gpunto@users.noreply.github.com>
2025-10-30 09:43:21 +0100

    Remove kotlin plugin application from java library

--- plugin/src/main/kotlin/io/getstream/android/StreamConventionPlugins.kt
+++ plugin/src/main/kotlin/io/getstream/android/StreamConventionPlugins.kt
@@ -46,7 +46,6 @@ class JavaLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply("java-library")
-            pluginManager.apply("org.jetbrains.kotlin.jvm")
 
             configureJava()
             configureKotlin()
