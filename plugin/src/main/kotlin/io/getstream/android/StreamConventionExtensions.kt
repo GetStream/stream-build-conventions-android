755bf6e10ced4f6533a16b3d8372b546d71e78a6
Gianmarco <47775302+gpunto@users.noreply.github.com>
2025-11-24 11:28:19 +0100

    Add coverage configuration to plugins (#7)

--- plugin/src/main/kotlin/io/getstream/android/StreamConventionExtensions.kt
+++ plugin/src/main/kotlin/io/getstream/android/StreamConventionExtensions.kt
@@ -15,6 +15,7 @@
  */
 package io.getstream.android
 
+import io.getstream.android.coverage.CoverageOptions
 import io.getstream.android.spotless.SpotlessOptions
 import javax.inject.Inject
 import org.gradle.api.Action
@@ -23,6 +24,7 @@ import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.findByType
+import org.gradle.kotlin.dsl.newInstance
 import org.gradle.kotlin.dsl.property
 
 /**
@@ -38,10 +40,16 @@ constructor(project: Project, objects: ObjectFactory) {
         objects.property<String>().convention(project.provider { project.rootProject.name })
 
     /** Spotless formatting configuration */
-    val spotless: SpotlessOptions = objects.newInstance(SpotlessOptions::class.java)
+    val spotless: SpotlessOptions = objects.newInstance<SpotlessOptions>()
 
     /** Configure Spotless formatting */
     fun spotless(action: Action<SpotlessOptions>) = action.execute(spotless)
+
+    /** Code coverage configuration */
+    val coverage: CoverageOptions = objects.newInstance<CoverageOptions>()
+
+    /** Configure code coverage */
+    fun coverage(action: Action<CoverageOptions>) = action.execute(coverage)
 }
 
 internal fun Project.createProjectExtension(): StreamProjectExtension =
